a4117ac9539714d1c4f9d74ae93ec119c1f31dcc
Edwin kullu <learningdevopssec@gmail.com>
2025-04-29 11:32:27 +0000

    update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "fastapi-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b"]
+  azs             = ["***a", "***b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -24,6 +24,12 @@ module "eks" {
   subnet_ids = module.vpc.private_subnets
 
   enable_irsa = true
+
+  iam_role_policy_attachments = {
+    example_policy = {
+      policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+    }
+  }
 
   eks_managed_node_groups = {
     default = {
