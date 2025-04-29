7d67683c80833ce67f003dada49574f888166854
Edwin kullu <learningdevopssec@gmail.com>
2025-04-29 11:36:22 +0000

    vpc add

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,17 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
@@ -10,9 +24,9 @@ module "eks" {
 
   enable_irsa = true
 
-  iam_role_additional_policies = {
-    eks_cluster = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  }
+  iam_role_additional_policies = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  ]
 
   eks_managed_node_groups = {
     default = {
