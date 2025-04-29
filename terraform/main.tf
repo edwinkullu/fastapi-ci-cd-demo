7fed02553f4b3a6406d485247fdd4a342fe1eaa9
Edwin kullu <learningdevopssec@gmail.com>
2025-04-29 11:34:24 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,3 @@
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
-
-  name = "fastapi-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["***a", "***b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "20.8.4"
@@ -25,10 +10,8 @@ module "eks" {
 
   enable_irsa = true
 
-  iam_role_policy_attachments = {
-    example_policy = {
-      policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-    }
+  iam_role_additional_policies = {
+    eks_cluster = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   }
 
   eks_managed_node_groups = {
