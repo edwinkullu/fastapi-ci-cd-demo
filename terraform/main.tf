14e46a240db33d883048a6a27ac81a0a056b14aa
Edwin kullu <learningdevopssec@gmail.com>
2025-04-29 11:43:31 +0000

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "3.19.0"
 
-  name = "my-vpc"
+  name = "fastapi-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -24,9 +24,9 @@ module "eks" {
 
   enable_irsa = true
 
-  iam_role_additional_policies = [
-    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  ]
+  iam_role_additional_policies = {
+    eks = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  }
 
   eks_managed_node_groups = {
     default = {
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,8 @@
 provider "aws" {
   region = var.region
 }
+
+variable "region" {
+  default = "ap-south-1"
+}
+
